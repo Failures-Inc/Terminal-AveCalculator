15ccab33484237f9e97b1c5aaf4152d65bb497b3
Crindere-Dev <69606716+Crindere-Dev@users.noreply.github.com>
2021-07-21 17:55:26 +0100

    yes

---
+++ Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+public class Program
+{
+    public static void Main()
+    {
+        double numb01;
+        double numb02;
+        double numb03;
+        int num = 3;
+        Console.Write("Input a number!: ");
+        numb01 = Convert.ToDouble(Console.ReadLine());
+        Console.Write("Input a second number!: ");
+        numb02 = Convert.ToDouble(Console.ReadLine());
+        Console.Write("Input a third number!: ");
+        numb03 = Convert.ToDouble(Console.ReadLine());
+        double result = (numb01 + numb02 + numb03) / num ;
+        Console.Write("= " + result);
+    }
+}
