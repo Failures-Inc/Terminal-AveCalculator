46f957142e1c0a8a52ec4f799141433c4aa70884
Crindere-Dev <69606716+Crindere-Dev@users.noreply.github.com>
2021-07-21 20:31:26 +0100

    faliure

--- Program.cs
+++ Program.cs
@@ -4,7 +4,22 @@ public class Program
 {
     public static void Main()
     {
-        double numb01;
+
+        // actual fucking code
+
+        Look();
+        Console.WriteLine("Hello! Welcome to the Average Calculator! \n \n Let's get Started!! \n");
+
+
+
+       Cal();
+        
+        
+    } public static void Cal(){
+
+        // calculator method
+        
+         double numb01;
         double numb02;
         double numb03;
         int num = 3;
@@ -14,7 +29,37 @@ public class Program
         numb02 = Convert.ToDouble(Console.ReadLine());
         Console.Write("Input a third number!: ");
         numb03 = Convert.ToDouble(Console.ReadLine());
+
+      
+
+
+
         double result = (numb01 + numb02 + numb03) / num ;
-        Console.Write("= " + result);
+        Console.Write($"The Average of {numb01}, {numb02} and {numb03} is {result}! \n \n");
+
+        Console.WriteLine(" Do you want to restart?");
+        // would you like to restart if statment
+        string sen = (Console.ReadLine());
+        if (sen == "yes"){
+            Console.WriteLine("--------------------------");
+        
+            Cal();
+
+        }else if (sen == "no"){
+            Environment.Exit(0);
+             
+        
+
+        }
+        
+
+    } public static void Look(){
+        Console.ForegroundColor = ConsoleColor.Blue;
+        Console.Title = "Average Calculator!";
+        
+        
+
+
+
     }
 }
